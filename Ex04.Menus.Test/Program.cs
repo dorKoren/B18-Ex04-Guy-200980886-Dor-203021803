d24c1cc8e9723f05a48b7bd2f03bdfa7454c1637
dor.koren <dor.koren@post.idc.ac.il>
2018-05-20 10:34:38 +0300

    Ex04.Menus.Test :  - Create & implement Program class.  - Create InterfacesTester class.  - Create DelegateTester class.

---
+++ Ex04.Menus.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using static Ex04.Menus.Test.InterfacesTester;
+using static Ex04.Menus.Test.DelegateTester;
+
+namespace Ex04.Menus.Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DemonstrateInterfacesMenu();
+            DemonstrateDelegatesMenu();
+        }
+    }
+}
