a703e64894f4d492731fcbe36c427fbcb715382b
dor.koren <dor.koren@post.idc.ac.il>
2018-05-21 18:07:14 +0300

    Ex04.Menus.Test :  * Implement  InterfaceTester

--- Ex04.Menus.Test/InterfacesTester.cs
+++ Ex04.Menus.Test/InterfacesTester.cs
@@ -1,16 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex04.Menus.Test
 {
-    class InterfacesTester
+    using Ex04.Menus.Interfaces;
+
+    public static class InterfacesTester
     {
         public static void DemonstrateInterfacesMenu()
         {
+            MainMenu mainMenu = buildInterfacesMenu();
+            mainMenu.Show();
+        }
 
+        private static MainMenu buildInterfacesMenu()
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.MainItemsCollection.Title = "*** Main Menu - Interfaces ***";
+
+            SubMenu dateAndTime = new SubMenu("Show Date/Time");
+            dateAndTime.Add(new ActionItem("Show Time", new Examples.Time()));
+            dateAndTime.Add(new ActionItem("Show Date", new Examples.Date()));
+            mainMenu.Add(dateAndTime);
+
+            SubMenu versionAndCapitals = new SubMenu("Version and Capitals");
+            versionAndCapitals.Add(new ActionItem("Count Capitals", new Examples.CountCapitals()));
+            versionAndCapitals.Add(new ActionItem("Show Version", new Examples.ShowVersion()));
+            mainMenu.Add(versionAndCapitals);
+
+            return mainMenu;
         }
     }
 }
